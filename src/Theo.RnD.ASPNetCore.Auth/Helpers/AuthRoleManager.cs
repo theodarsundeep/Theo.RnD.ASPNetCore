283d93e9c800777eb20d567bbad7b45bd305dcf5
sundeep.m <sundeep.m@RAIC003>
2016-04-20 12:52:37 +0530

    Add project files.

---
+++ src/Theo.RnD.ASPNetCore.Auth/Helpers/AuthRoleManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Theo.RnD.ASPNetCore.Auth.Helpers
+{
+    public class AuthRoleManager : RoleManager<IdentityRole>
+    {
+        public AuthRoleManager(
+            IRoleStore<IdentityRole> store,
+            IEnumerable<IRoleValidator<IdentityRole>> roleValidators,
+            ILookupNormalizer keyNormalizer,
+            IdentityErrorDescriber errors,
+            ILogger<RoleManager<IdentityRole>> logger,
+            IHttpContextAccessor contextAccessor)
+            : base(store, roleValidators, keyNormalizer, errors, logger, contextAccessor)
+        {
+
+        }
+    }
+}
---
+++ src/Theo.RnD.ASPNetCore.Auth/Helpers/InitRolesData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Theo.RnD.ASPNetCore.Auth.Helpers
+{
+    public static class InitRolesData
+    {
+        private static async Task EnsureRoleCreated(RoleManager<IdentityRole> roleManager, string roleName)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+        }
+
+        public static async Task EnsureRolesCreated(this RoleManager<IdentityRole> roleManager)
+        {
+            await EnsureRoleCreated(roleManager, "CPMAdmin");
+            await EnsureRoleCreated(roleManager, "CPMUser");
+            await EnsureRoleCreated(roleManager, "CPMSuperUser");
+        }
+    }
+}
